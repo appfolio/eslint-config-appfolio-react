6e5456378ac17c3992ad8152bf25773afb808f7a
Gary Thomas <gary.thomas@appfolio.com>
2016-10-11 14:09:52 -0700

    gt - Update appfolio base version, loosen proptypes

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports = {
   'extends': [
     'airbnb',
     'appfolio-base'
-  ]
+  ],
+  rules: {
+    'react/prop-types': 0 // TODO rule is adding support for validating declared only, relook then.
+  }
 }
