f47708c38826be1620942af0bacd66406269c18d
Aaron <aaron.mars@appfolio.com>
2022-02-07 11:22:05 -0800

    chore!: use latest `appfolio-base` in this config
    squashme

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
 module.exports = {
-  extends: ['airbnb', 'appfolio-base'],
-  env: {
-    browser: true
-  },
+  extends: ['airbnb', '@appfolio/eslint-config-appfolio-base'],
+  env: { browser: true },
   rules: {
-    'class-methods-use-this': 0,
-    'jsx-a11y/anchor-is-valid': [2, {
-      components: ['Link'],
-      specialLink: ['to']
-    }],
-
-    // Relaxes the prefer-default-export rule. We often have services / apis
-    // that start with a single named export which should not be the `default`.
-    'import/prefer-default-export': 0,
-
-    'react/forbid-prop-types': 0,
-    'react/jsx-filename-extension': 0,
-    'react/prefer-stateless-function': 0,
-    'react/prop-types': 1,
-    'react/require-default-props': 0,
-    'react/sort-comp': [1, {
-      order: [
-        'propTypes',
-        'defaultProps',
-        'static-methods',
-        'state',
-        'constructor',
-        'everything-else',
-        'lifecycle',
-        'render'
-      ]
-    }]
+    'class-methods-use-this': 'off',
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/prop-types': 'warn',
+    'react/require-default-props': 'off',
+    'react/sort-comp': [
+      'warn',
+      {
+        order: [
+          'propTypes',
+          'defaultProps',
+          'static-methods',
+          'state',
+          'constructor',
+          'everything-else',
+          'lifecycle',
+          'render'
+        ]
+      }
+    ]
   },
-  settings: {
-    'import/resolver': {
-      webpack: {}
-    }
-  }
+  settings: { 'import/resolver': { webpack: {} } }
 };
